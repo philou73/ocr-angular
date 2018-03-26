445b46f1974c58be5bd3a001e365c51dc32aa44f
Philou73 <guillemette.philippe@gmail.com>
2018-03-26 17:51:30 +0200

    RxJS
    Après la subscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core'; //On importe "OnInit"
+import { Observable, Subscription } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit { // On a ajouté "implements OnInit" pour implémenter l'interface
+
+// On a ajouté "implements OnInit" pour implémenter l'interface
+export class AppComponent implements OnInit { 
   title = 'app';
+	secondes: number;
+	counterSubscription : Subscription;
 
   constructor() {}
 
-	ngOnInit(){}
+	ngOnInit(){
+		const counter = Observable.interval(1000);
+		
+		//On souscrit aux événements de l'observable
+		this.counterSubscription = counter.subscribe(
+			(value) => {
+				this.secondes = value;
+			},
+			(error) => {
+				console.log('An error occured : ' + error);
+			},
+			() => {
+				console.log('Observable complete');
+			}
+		)
+	}
+	
+	ngOnDestroy(){
+		this.counterSubscription.unsubscribe();
+	}
 	
 }
